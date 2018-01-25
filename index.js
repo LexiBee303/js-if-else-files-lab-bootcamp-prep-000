a698dbfc4ce63e6876d655a7acc418f14939a4ee
Alexis Veri` <lexi.veri@gmail.com>
2018-01-25 23:13:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 var name = "Susan"
+<<<<<<< HEAD
 var height = "70"
+=======
+var height = "74"
+if (name === "Susan") {
+  console.log("The name is correct")
+} else {
+  console.log("Expected the name to be Susan")
+}
+>>>>>>> 8f16991768988ee9d687b8b440b51c9417d5f587
 
 
 // Don't worry about this
